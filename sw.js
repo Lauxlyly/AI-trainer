6f31e352e12c6a9e676be7f8d1dff4fd0e6c4f66
Lauxlyly <laurin85@gmail.com>
2026-04-19 15:56:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'treener-v24';
+const CACHE = 'treener-v25';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
